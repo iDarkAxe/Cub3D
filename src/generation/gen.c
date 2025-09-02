025a405d62644a65eaee1b3c297881739f4f1530
Pierre Pontet <ppontet@student.42lyon.fr>
2025-09-02 11:19:30 +0200

    maze gen returns new map if Successful

--- src/generation/gen.c
+++ src/generation/gen.c
@@ -6,7 +6,7 @@
 /*   By: ppontet <ppontet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 16:50:42 by rdesprez          #+#    #+#             */
-/*   Updated: 2025/08/09 12:43:05 by ppontet          ###   ########lyon.fr   */
+/*   Updated: 2025/09/02 11:10:07 by ppontet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,24 +21,27 @@ static float	random_float(float a)
 	return ((float)ft_rand() / (float)(FT_RAND_MAX / a));
 }
 
-int	cub_generate_dimensions(t_map_raoul **map, char *gen)
+t_map_raoul	*cub_generate_dimensions(char *gen)
 {
+	t_map_raoul	*map;
+
 	ft_srand(get_time_ms());
+	map = ft_calloc(1, sizeof(t_map_raoul));
 	if (!map)
-		return (0);
-	if (!*map)
-		*map = ft_calloc(1, sizeof(t_map_raoul));
-	if (!*map)
-		return (0);
-	if (!cub_parse_generation_arg(gen, &(*map)->width, &(*map)->height))
-		return (0);
-	if ((*map)->walls)
-		free((*map)->walls);
-	(*map)->walls = ft_calloc((*map)->width * (*map)->height, sizeof(int));
-	(*map)->start_pos = (t_pos2){(ft_rand() % (*map)->width) * 2 + 1, (ft_rand()
-			% (*map)->height) * 2 + 1};
-	(*map)->start_angle = random_float(PI * 2);
-	if ((*map)->walls == NULL)
-		return (0);
-	return (1);
+		return (NULL);
+	if (!cub_parse_generation_arg(gen, &map->width, &map->height))
+	{
+		free(map);
+		return (NULL);
+	}
+	map->walls = ft_calloc(map->width * map->height, sizeof(int));
+	if (map->walls == NULL)
+	{
+		free(map);
+		return (NULL);
+	}
+	map->start_pos = (t_pos2){(ft_rand() % map->width) * 2 + 1, (ft_rand()
+			% map->height) * 2 + 1};
+	map->start_angle = random_float(PI * 2);
+	return (map);
 }
