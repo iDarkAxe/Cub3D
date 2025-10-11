2750d2c93cad8619ec144975d4e815372a8b8a49
Pierre Pontet <ppontet@student.42lyon.fr>
2025-10-11 19:01:39 +0200

    fix: north and south texture inversion

--- src/hitside.c
+++ src/hitside.c
@@ -6,7 +6,7 @@
 /*   By: ppontet <ppontet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 12:05:26 by ppontet           #+#    #+#             */
-/*   Updated: 2025/08/07 19:19:39 by ppontet          ###   ########lyon.fr   */
+/*   Updated: 2025/10/11 18:49:18 by ppontet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ t_img	*hitside_texture(t_textures *tex, int hitside, const t_pos2 *step)
 	if (hitside)
 	{
 		if (step->y < 0)
-			return (&tex->north);
-		else
 			return (&tex->south);
+		else
+			return (&tex->north);
 	}
 	else
 	{
